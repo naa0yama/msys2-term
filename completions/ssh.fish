502edf814a753dd50f4c47df3314d5daea93a496
Naoki Aoyama <9667078+naa0yama@users.noreply.github.com>
2026-01-02 21:52:05 +0900

    chore: fix completions
    Signed-off-by: Naoki Aoyama <9667078+naa0yama@users.noreply.github.com>

--- completions/ssh.fish
+++ completions/ssh.fish
@@ -1,7 +1,25 @@
 #!/usr/bin/env fish
 
-# Inherit completions from the original ssh command
-complete --command ssh --wraps 'command ssh'
-
-# Add custom host completions from FSSH_SSH_CONF_DIR
+# Custom host completions from FSSH_SSH_CONF_DIR
 complete --command ssh --no-files --arguments '(__fssh_get_hosts)' --description 'SSH host'
+
+# Common SSH options (avoid --wraps recursion issue)
+complete --command ssh --short-option p --long-option port --description 'Port to connect to' --require-parameter
+complete --command ssh --short-option i --description 'Identity file' --require-parameter --force-files
+complete --command ssh --short-option l --long-option login_name --description 'Login name' --require-parameter
+complete --command ssh --short-option o --description 'SSH option' --require-parameter
+complete --command ssh --short-option F --description 'Config file' --require-parameter --force-files
+complete --command ssh --short-option J --description 'Jump host' --require-parameter
+complete --command ssh --short-option L --description 'Local port forward' --require-parameter
+complete --command ssh --short-option R --description 'Remote port forward' --require-parameter
+complete --command ssh --short-option D --description 'Dynamic port forward' --require-parameter
+complete --command ssh --short-option N --description 'No remote command'
+complete --command ssh --short-option T --description 'Disable pseudo-terminal'
+complete --command ssh --short-option t --description 'Force pseudo-terminal'
+complete --command ssh --short-option v --description 'Verbose mode'
+complete --command ssh --short-option q --description 'Quiet mode'
+complete --command ssh --short-option C --description 'Enable compression'
+complete --command ssh --short-option X --description 'Enable X11 forwarding'
+complete --command ssh --short-option Y --description 'Enable trusted X11 forwarding'
+complete --command ssh --short-option A --description 'Enable agent forwarding'
+complete --command ssh --short-option G --description 'Print configuration'
